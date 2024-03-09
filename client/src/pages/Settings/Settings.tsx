54725c3b341927c555eebe37ab447b48dd821ae1
Arnav Goel <arnav.goel1@outlook.com>
2024-03-10 04:06:11 +0800

    aaa

--- client/src/pages/Settings/Settings.tsx
+++ client/src/pages/Settings/Settings.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import "./Settings.css";
 
 const Settings = () => {
-    return <div>Settings</div>;
+    return (
+        <div className="container">
+            <div className="item">check</div>
+            <div className="item">check</div>
+        </div>
+    );
 };
 
 export default Settings;
