bab37b96e3e5747b2fba4c733d2832cde39ea5d6
Arnav Goel <arnav.goel1@outlook.com>
2024-03-12 15:29:04 +0800

    added noti list

--- client/src/components/NotificationList/Notifcation/Notification.tsx
+++ client/src/components/NotificationList/Notifcation/Notification.tsx
@@ -3,12 +3,14 @@ import "./Notification.css";
 import Cross from "./Cross/Cross";
 const Notification = () => {
     return (
-        <span className="notification-box">
+        <div className="notification-box">
             <span className="notiText">
-                LOLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLlll
+                To solve this issue and ensure that such long strings wrap
+                within their container, you just checking if it still continues
+                working or not
             </span>
             <Cross />
-        </span>
+        </div>
     );
 };
 
--- client/src/components/NotificationList/NotificationList.tsx
+++ client/src/components/NotificationList/NotificationList.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Notification from "./Notifcation/Notification";
+import "./NotificationList.css";
 
 const NotificationList = () => {
     return (
-        <>
+        <div className="notification-list">
             <Notification />
             <Notification />
-        </>
+        </div>
     );
 };
 
